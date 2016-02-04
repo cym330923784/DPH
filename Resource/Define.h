2b2291115495d924e0603d2008562f1e7b39340f
cym <330923784@qq.com>
2016-02-04 15:54:42 +0800

    no message

--- Resource/Define.h
+++ Resource/Define.h
@@ -12,9 +12,9 @@
 #define Screen  [UIScreen mainScreen].bounds.size
 
 //外网
-//#define BASE_NET @"http://112.124.123.202:8080/dph/rest/"
+#define BASE_NET @"http://112.124.123.202:8080/dph/rest/"
 //内网
-#define BASE_NET @"http://192.168.2.109:8080/rest/"
+//#define BASE_NET @"http://192.168.2.109:8080/rest/"
 //七牛图片域名
 #define QN_ImageUrl @"http://7xqjf2.com1.z0.glb.clouddn.com/"
 
